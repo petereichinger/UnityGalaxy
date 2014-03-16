eab514a5e6f8d371fb860095e0b4a012fca874be
Peter Eichinger <peter.eichinger@gmail.com>
2014-03-16 12:15:07 +0100

    Galaxy is split up in strange arms

--- Assets/Scripts/GalaxyGeneration.cs
+++ Assets/Scripts/GalaxyGeneration.cs
@@ -8,11 +8,26 @@ public class GalaxyGeneration {
 	private int starCount;
 	private int? seed;
 	private float size;
-	public GalaxyGeneration(ParticleSystem particleSystem, int starCount, float size, int? seed = null) {
+	private int numberOfArms;
+	private float armSeperationAngle;
+	private float armOffsetMax;
+	public GalaxyGeneration(ParticleSystem particleSystem,
+		int? seed = null,
+		float size = 1,
+		int starCount = 10000,
+		int numberOfArms = 5,
+		float armOffsetMax = 0.5f) {
+
+		// Set fields according to parameters
 		this.seed = seed;
 		this.galaxyParticles = particleSystem;
 		this.starCount = starCount;
 		this.size = size;
+		this.numberOfArms = numberOfArms;		
+		this.armOffsetMax = armOffsetMax;
+
+		// Calculate helpers
+		this.armSeperationAngle = 2 * Mathf.PI / numberOfArms;
 	}
 
 	public void Generate() {
@@ -20,12 +35,19 @@ public class GalaxyGeneration {
 			Random.seed = this.seed.Value;
 		}
 		galaxyParticles.Clear();// Remove all particles from galaxyParticles
-		for (int i = 0; i < this.starCount; i++) {
-			float distance = Random.value * size;
-			float angle = Random.value * 2 * Mathf.PI;
+		for (int i = 0; i < this.starCount; i++) {	// for loop for stars
+			float distance = Random.value * size;	// random distance between 0 and size
+			float angle = Random.value * 2 * Mathf.PI; // random angle
+			float armOffset = Random.value * armOffsetMax;	// arm offset angle so it spreads out
+			armOffset = armOffset - armOffsetMax / 2;
+			armOffset = armOffset * (1/ distance);
+			angle = ((int)(angle / armSeperationAngle)) * armSeperationAngle + armOffset;
 
+			//Calculate position using cos and sin
 			Vector3 position = new Vector3(Mathf.Cos(angle) * distance, Mathf.Sin(angle) * distance);
 
+
+			// emit a particle at the position
 			galaxyParticles.Emit(position, Vector3.zero, 0.1f, float.PositiveInfinity, Color.white);
 		}
 	}
--- Assets/Scripts/GalaxyGeneratorComponent.cs
+++ Assets/Scripts/GalaxyGeneratorComponent.cs
@@ -4,15 +4,18 @@ using System.Collections.Generic;
 
 public class GalaxyGeneratorComponent : MonoBehaviour {
 	public ParticleSystem galaxyParticleSystem;
-	public int numberOfStars;
+
 	public bool useSeed;
 	public int seed;
-	public float size;
+	public int numberOfStars;
+	public float galaxySize;
+	public int numberOfArms;
 	public void Update() {
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			int? seedToUse = useSeed ? (int?)seed : null;
 
-			GalaxyGeneration galGen = new GalaxyGeneration(galaxyParticleSystem, numberOfStars, size, seedToUse);
+			GalaxyGeneration galGen = new GalaxyGeneration(galaxyParticleSystem, seedToUse, galaxySize, numberOfStars,
+				numberOfArms);
 
 			galGen.Generate();
 		}
