e35984fad8a2b26700b636ef8de8a36413d25ba4
Peter Eichinger <peter.eichinger@gmail.com>
2014-03-16 11:54:54 +0100

    FIrst step to galaxy generation
    Creates a random distribution of stars

---
+++ Assets/Scripts/GalaxyGeneration.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GalaxyGeneration {
+
+	private ParticleSystem galaxyParticles;
+	private int starCount;
+	private int? seed;
+	private float size;
+	public GalaxyGeneration(ParticleSystem particleSystem, int starCount, float size, int? seed = null) {
+		this.seed = seed;
+		this.galaxyParticles = particleSystem;
+		this.starCount = starCount;
+		this.size = size;
+	}
+
+	public void Generate() {
+		if (seed.HasValue) {
+			Random.seed = this.seed.Value;
+		}
+		galaxyParticles.Clear();// Remove all particles from galaxyParticles
+		for (int i = 0; i < this.starCount; i++) {
+			float distance = Random.value * size;
+			float angle = Random.value * 2 * Mathf.PI;
+
+			Vector3 position = new Vector3(Mathf.Cos(angle) * distance, Mathf.Sin(angle) * distance);
+
+			galaxyParticles.Emit(position, Vector3.zero, 0.1f, float.PositiveInfinity, Color.white);
+		}
+	}
+}
---
+++ Assets/Scripts/GalaxyGeneratorComponent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GalaxyGeneratorComponent : MonoBehaviour {
+	public ParticleSystem galaxyParticleSystem;
+	public int numberOfStars;
+	public bool useSeed;
+	public int seed;
+	public float size;
+	public void Update() {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			int? seedToUse = useSeed ? (int?)seed : null;
+
+			GalaxyGeneration galGen = new GalaxyGeneration(galaxyParticleSystem, numberOfStars, size, seedToUse);
+
+			galGen.Generate();
+		}
+	}
+
+
+}
